e0fa524975789183118a31423ac91a6ede0df2d2
SufiyanRehan <mdsufiyan.momin7@gmail.com>
2025-04-17 12:30:46 +0530

    added first code MoveZero problem.

---
+++ LeetCodePractice/src/easy/MoveZero.java
@@ -0,0 +1,36 @@
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//Note that you must do this in-place without making a copy of the array.
+//Example 1:
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+//Example 2:
+//Input: nums = [0]
+//Output: [0]
+
+import java.util.Arrays;
+
+public class MoveZero {
+
+public static  void main (String[] args){
+    int[] nums= {0,1,0,3,12};
+     moveZero(nums);
+    System.out.println(Arrays.toString(nums));
+}
+
+    public static void moveZero(int[] nums) {
+
+    int insertPosition=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i] !=0){
+        nums[insertPosition]=nums[i];
+            insertPosition++;
+        }
+    }
+    while(insertPosition<nums.length){
+        nums[insertPosition]=0;
+        insertPosition++;
+    }
+
+}
+
+}
